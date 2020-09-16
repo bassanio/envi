30df473fdb6fbc70cc8456cf96efa2ec1af8e193
bassanio <yoursbassanio@gmail.com>
2020-09-16 13:01:37 +0400

    Add files via upload

---
+++ CreateEnv.R
@@ -0,0 +1,22 @@
+message("Update and install needed Ubuntu packages")
+system("apt update")
+system("apt install -y libmysqlclient-dev libudunits2-dev libgeos-dev libgdal-dev libcairo2-dev")
+
+downloadGDriveFile <- function(id, out){
+  system(paste0(
+    'wget --continue --load-cookies /tmp/cookies.txt ',
+    '"https://docs.google.com/uc?export=download&confirm=$(wget ',
+    '--quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ',
+    "'https://docs.google.com/uc?export=download&id=", id, "' -O- | ",
+    "sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=", id, '" ',
+    "-O ", out, " && rm -rf /tmp/cookies.txt"), intern=TRUE)
+}
+
+message("Retrieve data for tutorials")
+message("Download R package cache")
+downloadGDriveFile(out="PAGData.tar.gz", id="1W2TJYI1Fc_d7DlYgTLFxaoxeFZ95jZec")
+message("Done")
+
+
+message("Unzipping Data")
+system("tar -xvzf PAGData.tar.gz -C ./ ", intern=TRUE)
