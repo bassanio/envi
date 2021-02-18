9fc7f2aad68ad543c1dac453028d3fcffe4cc987
bassanio <yoursbassanio@gmail.com>
2021-02-18 19:46:36 +0400

    Create 86Sample.R

---
+++ 86Sample.R
@@ -0,0 +1,19 @@
+downloadGDriveFile <- function(id, out){
+  system(paste0(
+    'wget --continue --load-cookies /tmp/cookies.txt ',
+    '"https://docs.google.com/uc?export=download&confirm=$(wget ',
+    '--quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ',
+    "'https://docs.google.com/uc?export=download&id=", id, "' -O- | ",
+    "sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=", id, '" ',
+    "-O ", out, " && rm -rf /tmp/cookies.txt"), intern=TRUE)
+}
+
+message("Retrieve data for tutorials")
+message("Download Data")
+downloadGDriveFile(out="Covid19_DATA.tar.gz", id="1uUJ_IJiLGHjW5gmkUx1VrjCtrBotHZNW")
+
+
+message("Unzipping Data")
+system("tar -xzf Covid19_DATA.tar.gz -C ./ ", intern=TRUE)
+
+message("Done")
