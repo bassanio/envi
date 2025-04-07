4bc7840a21f6f71b696988224b5abf684e3ba7c4
bassanio <yoursbassanio@gmail.com>
2025-04-07 15:08:39 +0400

    Create CBH2025.R

---
+++ CBH2025.R
@@ -0,0 +1,35 @@
+#message("Update and install needed Ubuntu packages")
+#system("apt update")
+#system("apt install -y libmysqlclient-dev libudunits2-dev libgeos-dev libgdal-dev libcairo2-dev")
+
+downloadGDriveFile <- function(id, out){
+  system(paste0(
+    'wget --continue --load-cookies /tmp/cookies.txt ',
+    '"https://docs.google.com/uc?export=download&confirm=$(wget ',
+    '--quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ',
+    "'https://docs.google.com/uc?export=download&id=", id, "' -O- | ",
+    "sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=", id, '" ',
+    "-O ", out, " && rm -rf /tmp/cookies.txt"), intern=TRUE)
+}
+
+message("Retrieve data for tutorials")
+message("Download Data")
+downloadGDriveFile(out="Training_Dataset.tar.gz", id="1FfVXISnCJQLaK7UWlPvCiukNBO0bCkHN")
+
+
+message("Unzipping Data")
+system("tar -xvzf Training_Dataset.tar.gz -C ./ ", intern=TRUE)
+
+message("Done")
+
+
+message("Download and Install R package")
+install.packages("qqman")
+install.packages("IRdisplay")
+
+library(IRdisplay)
+
+
+
+
+message("Done")
